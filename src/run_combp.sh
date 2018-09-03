75eb5e86bb86bf8ce47390666479a9ff1fe0f321
kwesterman <ken.e.westerman@gmail.com>
2018-09-03 12:50:36 -0400

    Update comb-p script to make use of Python 2.7 explicit.

--- src/run_combp.sh
+++ src/run_combp.sh
@@ -6,9 +6,9 @@ RAW_BED=${DATASET}Res.bed
 
 cd ../int
 
-python $CPV_DIR/acf.py -d 1:1000:$STEP_SIZE -c $P_COL $RAW_BED > acf_$DATASET.txt
-python $CPV_DIR/slk.py --acf acf_${DATASET}.txt -c $P_COL ${DATASET}Res.bed > acf_${DATASET}.bed
-python $CPV_DIR/peaks.py --dist 500 --seed 0.1 acf_${DATASET}.bed > regions_${DATASET}.bed
-python $CPV_DIR/region_p.py -p $RAW_BED -r regions_${DATASET}.bed -s $STEP_SIZE -c $P_COL > regions.sig_${DATASET}.bed
+python2.7 $CPV_DIR/acf.py -d 1:1000:$STEP_SIZE -c $P_COL $RAW_BED > acf_$DATASET.txt
+python2.7 $CPV_DIR/slk.py --acf acf_${DATASET}.txt -c $P_COL ${DATASET}Res.bed > acf_${DATASET}.bed
+python2.7 $CPV_DIR/peaks.py --dist 500 --seed 0.1 acf_${DATASET}.bed > regions_${DATASET}.bed
+python2.7 $CPV_DIR/region_p.py -p $RAW_BED -r regions_${DATASET}.bed -s $STEP_SIZE -c $P_COL > regions.sig_${DATASET}.bed
 
 cd ../src
