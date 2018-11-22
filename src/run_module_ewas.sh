e0c5c6bf0933cbe32e063f502124baa4ced2462c
kwesterman <ken.e.westerman@gmail.com>
2018-11-22 01:51:08 -0500

    Separate supplementary info for module EWAS paper into separate report.

--- src/run_module_ewas.sh
+++ src/run_module_ewas.sh
@@ -7,4 +7,7 @@
 #SBATCH -o module_ewas-%A.out
 
 module load R/3.4.3
-time Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/opt/shared/RStudio/0.98/bin/pandoc'); rmarkdown::render('module_ewas.Rmd', output_dir='../doc/module_ewas/')"
+time Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/opt/shared/RStudio/0.98/bin/pandoc'); "\
+"rmarkdown::render('module_ewas.Rmd', output_dir='../doc/module_ewas/')"
+time Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/opt/shared/RStudio/0.98/bin/pandoc'); "\
+"rmarkdown::render('module_ewas_supp.Rmd', output_dir='../doc/module_ewas/')"\
